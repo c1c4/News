a8e0d076ed4e1e93ff6a740fb245ea2549eb8ef5
Felipe Arado Pompeu <felipe.pompeu@adde.io>
2019-05-18 23:03:13 -0300

    Atualização
    Feito a atualização da chave do NewsAPI

--- app/src/main/java/dev/dextra/newsapp/Constants.kt
+++ app/src/main/java/dev/dextra/newsapp/Constants.kt
@@ -1,7 +1,7 @@
 package dev.dextra.newsapp
 
 val API_KEY_HEADER_NAME = "X-Api-Key"
-val API_KEY = "bd085612580ff475ab010d7720e8aaad"
+val API_KEY = "b494ffdab38e4b7f856355e02d22101b"
 val BASE_URL = "https://newsapi.org"
 val ICON_LOCATOR_URL = "https://icon-locator.herokuapp.com/icon?url=%s&size=30..60..150"
 val IMAGE_LOCATOR_URL = "https://icon-locator.herokuapp.com/icon?url=%s&size=100..300..500"
